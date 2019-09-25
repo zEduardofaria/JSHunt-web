09b5c7e5456d95c79598fd2457cfa5cb7de2c89f
Eduardo Faria <z.eduardofaria@gmail.com>
2019-09-25 09:36:55 -0300

    Hello hunt

---
+++ hunt/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
